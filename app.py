bc76f78506f42aaff5692df57ca31bc06662b412
Douglas Damasio <douglasdamasio18@gmail.com>
2024-05-06 22:28:35 -0300

    remove @cross_origin

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ from flask import Flask, render_template
 from datasources.team import DataSourceTeam
 from exceptions import TeamNotFound
 from services import CartolaService
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
-@cross_origin
 def msg_hello():
     return render_template('index.html')
 
 @app.route('/teams')
-@cross_origin
 def show_teams():
     all_teams = DataSourceTeam.get_all_teams()
     
@@ -27,7 +25,6 @@ def show_teams():
     return [team.serialize() for team in ordered_teams]
 
 @app.route('/team/<nickname>')
-@cross_origin
 def show_team_by_player_nickname(nickname):
     try:
         team = DataSourceTeam.get_team_by_nickname(nickname)
@@ -36,6 +33,5 @@ def show_team_by_player_nickname(nickname):
         return str(e), 404
 
 @app.route('/round')
-@cross_origin
 def show_current_round():
     return CartolaService().get_current_round().serialize()
