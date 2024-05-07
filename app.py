d3377f8016592abcde65e3fb9a26654d9c30d277
Thiago Teixeira Nunes <thiago@macOs.local>
2024-05-06 22:05:00 -0300

    feat: add cors origin headers

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template
 from datasources.team import DataSourceTeam
 from exceptions import TeamNotFound
 from services import CartolaService
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin
 def msg_hello():
     return render_template('index.html')
 
 @app.route('/teams')
+@cross_origin
 def show_teams():
     all_teams = DataSourceTeam.get_all_teams()
     
@@ -22,6 +27,7 @@ def show_teams():
     return [team.serialize() for team in ordered_teams]
 
 @app.route('/team/<nickname>')
+@cross_origin
 def show_team_by_player_nickname(nickname):
     try:
         team = DataSourceTeam.get_team_by_nickname(nickname)
@@ -30,5 +36,6 @@ def show_team_by_player_nickname(nickname):
         return str(e), 404
 
 @app.route('/round')
+@cross_origin
 def show_current_round():
     return CartolaService().get_current_round().serialize()
